13f4fa5910a00ffc41ab78dfa5c3e23e6cba356a
Zhongy95 <1007363681@qq.com>
2020-06-15 23:05:36 +0800

    为就诊系统打点基础

---
+++ src/main/java/com/group7/bus/controller/DoctorController.java
@@ -0,0 +1,22 @@
+package com.group7.bus.controller;
+
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bus/doctor")
+@RequiresRoles("DOCTOR")
+public class DoctorController {
+
+    /** 跳转到排队治疗 */
+    @RequestMapping("toRegisterQueue")
+    public String toRegisterQueue() { return "business/record/doctorRegisterQueue"; }
+
+    /** 跳转到病历查看 */
+    @RequestMapping("toRecordManager")
+    public String toRecordManager() { return "business/record/doctorRecordManager"; }
+
+
+
+}

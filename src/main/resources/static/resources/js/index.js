051e739723b0ad0b7cd038f1d327df22cb204a26
nooblyh <472897494@qq.com>
2020-06-17 21:20:19 +0800

    删掉尴尬的美女照片 删掉页面上无关内容

--- src/main/resources/static/resources/js/index.js
+++ src/main/resources/static/resources/js/index.js
@@ -140,28 +140,3 @@ layui.use(['bodyTab','form','element','layer','jquery'],function(){
 function addTab(_this){
 	tab.tabAdd(_this);
 }
-
-//捐赠弹窗
-function donation(){
-	layer.tab({
-		area : ['260px', '367px'],
-		tab : [{
-			title : "微信",
-			content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='images/wechat.jpg'></div>"
-		},{
-			title : "支付宝",
-			content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='images/alipay.jpg'></div>"
-		}]
-	})
-}
-
-//图片管理弹窗
-function showImg(){
-    $.getJSON('json/images.json', function(json){
-        var res = json;
-        layer.photos({
-            photos: res,
-            anim: 5
-        });
-    });
-}

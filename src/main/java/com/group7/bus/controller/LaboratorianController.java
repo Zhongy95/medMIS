a3187f6ad8b6fcf1fbf9c964a044b46f5bf88411
Zhongy95 <1007363681@qq.com>
2020-06-20 16:02:19 +0800

    Exam系统的基础

---
+++ src/main/java/com/group7/bus/controller/LaboratorianController.java
@@ -0,0 +1,27 @@
+package com.group7.bus.controller;
+
+
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bus/laboratorian")
+@RequiresRoles("LABORATORIAN")
+public class LaboratorianController {
+
+
+    /**跳转到检查挂号 */
+    @RequestMapping("toExamRegister")
+    public String toExamRegister(){return  "business/exam/doctorExamRegister";}
+
+    /**跳转到检查挂号排队 */
+    @RequestMapping("toExamRegisterQueue")
+    public String toExamRegisterQueue(){return  "business/exam/doctorExamQueue";}
+
+    /**跳转到检查挂号排队 */
+    @RequestMapping("toExamManager")
+    public String toExamManage(){return  "business/exam/doctorExamManager";}
+
+
+}
